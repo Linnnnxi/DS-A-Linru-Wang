dc2d00d26bfc6d436e57c03f321b856dbb0d23ab
Linnnnxi <x23177152@student.ncirl.ie>
2025-03-15 18:39:22 +0000

    First commit

---
+++ src/main/java/ca/bloodapp/BloodApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.bloodapp;
+
+/**
+ *
+ * @author lenovo
+ */
+public class BloodApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
